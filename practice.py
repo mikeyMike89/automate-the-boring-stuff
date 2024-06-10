2f146070a7c53a03221567635479b69cb9bfaab5
Michael <dev.mpetan@gmail.com>
2024-06-10 16:47:37 -0400

    This is a test.

---
+++ practice.py
@@ -0,0 +1,15 @@
+def collatz(number):
+    if number % 2 == 0:
+        result = number // 2
+    else:
+        result = 3 * number + 1
+    print(result)
+    return result
+    
+user_input = input("Please enter an integer: ")
+number = int(user_input)
+
+while number != 1:
+    number = collatz(number)
+
+
